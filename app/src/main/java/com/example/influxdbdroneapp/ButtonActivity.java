d8a401dc8bed65dea79f1b08de2f464c5febb9ce
Lethal <l.letal244@gmail.com>
2022-09-12 15:03:36 +0200

    Fully functional part + app icon

--- app/src/main/java/com/example/influxdbdroneapp/ButtonActivity.java
+++ app/src/main/java/com/example/influxdbdroneapp/ButtonActivity.java
@@ -8,7 +8,6 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.influxdb.client.InfluxDBClient;
 import com.influxdb.client.WriteApiBlocking;
@@ -23,50 +22,43 @@ public class ButtonActivity extends AppCompatActivity {
         setContentView(R.layout.activity_button);
     }
 
-    Runnable data_collection = new Runnable() {
-        @Override
-        public void run() {
-            WriteApiBlocking writeApi = setup_client.getWriteApiBlocking();
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-            while (bolswitch){
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                influxdbclass.pushfloat(writeApi, "Dji", djidatacollect.drone_name, "Basic_metrics", "VelocityX", djidatacollect.velocityX());
-                influxdbclass.pushfloat(writeApi, "Dji", djidatacollect.drone_name, "Basic_metrics", "VelocityY", djidatacollect.velocityY());
-                influxdbclass.pushfloat(writeApi, "Dji", djidatacollect.drone_name, "Basic_metrics", "VelocityZ", djidatacollect.velocityZ());
-                influxdbclass.pushint(writeApi, "Dji", djidatacollect.drone_name, "Basic_metrics", "FlightTime", djidatacollect.flight_time());
-                influxdbclass.pushint(writeApi, "Dji", djidatacollect.drone_name, "Battery_metrics", "Battery_percentage", djidatacollect.battery_percent());
-                influxdbclass.pushint(writeApi, "Dji", djidatacollect.drone_name, "Battery_metrics", " Battery_current", djidatacollect.battery_current());
-                influxdbclass.pushint(writeApi, "Dji", djidatacollect.drone_name, "Battery_metrics", "Battery_voltage", djidatacollect.battery_voltage());
+    Runnable data_collection = () -> {
+        WriteApiBlocking writeApi = setup_client.getWriteApiBlocking();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        while (bolswitch){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            influxdbclass.pushfloat(writeApi, "Dji", djidatacollect.drone_name, "Basic_metrics", "VelocityX", djidatacollect.velocityX());
+            influxdbclass.pushfloat(writeApi, "Dji", djidatacollect.drone_name, "Basic_metrics", "VelocityY", djidatacollect.velocityY());
+            influxdbclass.pushfloat(writeApi, "Dji", djidatacollect.drone_name, "Basic_metrics", "VelocityZ", djidatacollect.velocityZ());
+            influxdbclass.pushint(writeApi, "Dji", djidatacollect.drone_name, "Basic_metrics", "FlightTime", djidatacollect.flight_time());
+            influxdbclass.pushint(writeApi, "Dji", djidatacollect.drone_name, "Battery_metrics", "Battery_percentage", djidatacollect.battery_percent());
+            influxdbclass.pushint(writeApi, "Dji", djidatacollect.drone_name, "Battery_metrics", " Battery_current", djidatacollect.battery_current());
+            influxdbclass.pushint(writeApi, "Dji", djidatacollect.drone_name, "Battery_metrics", "Battery_voltage", djidatacollect.battery_voltage());
         }
-
     };
-    Runnable debug = new Runnable() {
-        @Override
-        public void run() {
-            WriteApiBlocking writeApi = setup_client.getWriteApiBlocking();
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-            while(bolswitch){
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                influxdbclass.pushint(writeApi, "Dji", "Debug_drone", "Debug_cycle", "debug", 2);
-                influxdbclass.pushint(writeApi, "Dji", "Debug_drone", "Debug_cycle", "debug", 2);
-                influxdbclass.pushint(writeApi, "Dji", "Debug_drone", "Debug_cycle", "debug", 2);
-                influxdbclass.pushint(writeApi, "Dji", "Debug_drone", "Debug_cycle", "debug", 2);
-                influxdbclass.pushint(writeApi, "Dji", "Debug_drone", "Debug_cycle", "debug", 2);
-                influxdbclass.pushint(writeApi, "Dji", "Debug_drone", "Debug_cycle", "debug", 2);
-                influxdbclass.pushint(writeApi, "Dji", "Debug_drone", "Debug_cycle", "debug", 2);
-
+    public int debug_fun(){
+        return(0);
+    }
+    Runnable debug = () -> {
+        WriteApiBlocking writeApi = setup_client.getWriteApiBlocking();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        while(bolswitch){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            influxdbclass.pushint(writeApi, "Dji", "Debug_drone", "Debug_cycle", "debug", debug_fun());
+            influxdbclass.pushint(writeApi, "Dji", "Debug_drone", "Debug_cycle", "debug", debug_fun());
+            influxdbclass.pushint(writeApi, "Dji", "Debug_drone", "Debug_cycle", "debug", debug_fun());
+
+
         }
     };
     @SuppressLint("SetTextI18n")
